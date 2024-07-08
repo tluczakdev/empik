30b43c0225ffa573fc936b200452a2fb20c36036
Tomasz Łuczak <tluczak.dev@gmail.com>
2024-07-08 14:53:05 +0200

    init commit

---
+++ src/main/java/org/interview/empik/EmpikApplication.java
@@ -0,0 +1,13 @@
+package org.interview.empik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmpikApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/interview/empik/EmpikApplicationTests.java
@@ -0,0 +1,13 @@
+package org.interview.empik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpikApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
